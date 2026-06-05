97a3f54677d4656279f120a837013ddb64088b38
Raphael Groarke <rgroarke@gmail.com>
2026-06-05 21:56:13 +0200

    added stuff

--- Scripts/flowers.gd
+++ Scripts/flowers.gd
@@ -8,16 +8,16 @@ var currentFlower = "Sunflower" # default flower (we will add code later on to c
 var flowerDic = {"Sunflower": [0, 6, 1]} #Sunflower, at position 0 in the list, with 5 growth stages, price
 
 
-func place_flower(tile, name, stage):
+func place_flower(tile, flowName, stage):
 	# if there is no name then nothnig is currently planted so we plant our current default flower
-	if name == "":
+	if flowName == "":
 		set_cell(tile, flowerDic[currentFlower][0], Vector2i(0, 0), 0)
 		return [currentFlower, 0]
 	
 	
 	# if the new stage is under the growth limit proceed --> handle else for selling to erase the cell
-	if stage < flowerDic[name][1]:
-		set_cell(tile, flowerDic[name][0], Vector2i(stage, 0), 0)
+	if stage < flowerDic[flowName][1]:
+		set_cell(tile, flowerDic[flowName][0], Vector2i(stage, 0), 0)
 		return [currentFlower, stage]
 	else:
 		erase_cell(tile)
