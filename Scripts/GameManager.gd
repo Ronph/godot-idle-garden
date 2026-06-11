6e1f6b4ba3586de82c973b6af1c024922ae8f643
Raphael Groarke <rgroarke@gmail.com>
2026-06-11 14:43:11 +0200

    added basket variable to gamemanager

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -5,6 +5,7 @@ signal gained_coins(int)
 var coins : int
 var unlocked_flowers: Array = ["Sunflower"]
 var harvestSize = 5
+var basketSize = 10
 
 var flowerDic = {"Sunflower": [0, 6, 1, 0], #Sunflower, at position 0 in the list, with 5 growth stages, sell price
 				 "Cabbage": [1, 4, 2, 5]} 
