ff09ce33908fab789f16bb6f9e48eff6a0c7917c
Raphael Groarke <rgroarke@gmail.com>
2026-06-11 12:49:06 +0200

    fixed error when harvesting empty tile, and fixed error with harvesting plant early making the plant go down a stage

--- Scripts/flowers.gd
+++ Scripts/flowers.gd
@@ -4,12 +4,14 @@ var currentFlower : String:
 	get: return GameManager.currentFlower # default flower (we will add code later on to change the current default)
 
 func harvest_flower(tile, flowName, stage, x, y):
+	if flowName == "":
+		return [flowName, stage, x, y]
 	if stage == GameManager.flowerDic[flowName][1] - 1:
 		erase_cell(tile)
 		GameManager.gain_coins(GameManager.flowerDic[flowName][2])
 		return ["", -1, x, y]
 	else:
-		return [flowName, stage - 1 , x, y]
+		return [flowName, stage, x, y]
 
 func place_random_flower(tile, _flowName, _stage, x, y):
 	set_cell(tile, GameManager.flowerDic[currentFlower][0], Vector2i(0, 0), 0)
