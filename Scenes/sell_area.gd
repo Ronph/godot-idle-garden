9903b4a848177b007b31350e32cdc49c77bf78a7
Raphael Groarke <rgroarke@gmail.com>
2026-06-16 21:22:24 +0200

    removed debug message

--- Scenes/sell_area.gd
+++ Scenes/sell_area.gd
@@ -4,5 +4,5 @@ extends Control
 
 func _can_drop_data(_at_position, data):
 	return data is Dictionary and data.get("type") == "basket"
-func _drop_data(_at_position, data):
+func _drop_data(_at_position, _data):
 	GameManager.sellBasket()
